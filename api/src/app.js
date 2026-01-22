c1ef4cbedc0732f27c17ade180186647ce6dde12
Shemz-ux <shemaiahnguessan@outlook.com>
2026-01-22 15:43:51 +0000

    Revert CORS to allow all origins

--- api/src/app.js
+++ api/src/app.js
@@ -4,18 +4,7 @@ import apiRouter from "./routes/api-routers.js";
 import { customError, psqlError, serverError } from "./middleware/errorHandlers.js";
 const app = express();
 
-const corsOptions = {
-  origin: [
-    'http://localhost:5173', // Vite dev server
-    'http://localhost:5174', // Alternative Vite port
-    'https://fop-app.vercel.app' // Production frontend
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
