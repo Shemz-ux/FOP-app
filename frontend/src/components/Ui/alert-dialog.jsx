67e6c6929d072abf0fa9e4cbcae25be81cdc7beb
Shemz-ux <shemaiahnguessan@outlook.com>
2026-01-22 14:59:50 +0000

    Fix all case-sensitivity issues: Button import and SortDropdown directory

--- frontend/src/components/Ui/alert-dialog.jsx
+++ frontend/src/components/Ui/alert-dialog.jsx
@@ -1,7 +1,7 @@
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 
 import { cn } from "./utils";
-import { buttonVariants } from "./button";
+import { buttonVariants } from "./Button";
 
 function AlertDialog(props) {
   return (
